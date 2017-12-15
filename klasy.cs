f873eeee763be65d2b97cb8b9c8f62e03b010191
lukinysa <33897947+lukinysa@users.noreply.github.com>
2017-12-15 16:52:16 +0100

    Add files via upload

---
+++ klasy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Pracownik
+    {
+        public static int Count = 0;
+        public string name;
+        public int age;
+        public int wage;
+
+        public Pracownik(string _name, int _age, int _wage)
+        {
+            name = _name;
+            age = _age;
+            wage = _wage;
+            Count++;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Imie: " + name);
+            if (age >= 65)
+            {
+                Console.WriteLine("Wiek: " + age + " Powinnien iść na emeryturę");
+            }
+            else
+            {
+                Console.WriteLine("Wiek: " + age);
+            }
+        }
+        void Podwyzka()
+        {
+            Console.WriteLine(wage + " Chcę podwyżkę!");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pracownik[] array = new Pracownik[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Podaj imię pracownika: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Podaj wiek pracownika: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Podaj pensję pracownika: ");
+                int wage = Convert.ToInt32(Console.ReadLine());
+                Pracownik Pracownik = new Pracownik(name, age, wage);
+                array[i] = Pracownik;
+            }
+            Console.ReadKey();
+        }
+    }
+}
